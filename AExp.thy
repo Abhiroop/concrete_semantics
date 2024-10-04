642a37c76c0ccdb039749f4aec982388777f65b0
Abhiroop <asiamgenius@gmail.com>
2024-10-04 20:31:24 +0200

    Add 3.10

--- AExp.thy
+++ AExp.thy
@@ -407,6 +407,40 @@ theorem "is_nnf b \<Longrightarrow> is_dnf (dnf_of_nnf b)"
   apply (auto simp add: isdnf_andb)
   done
 
+(* Ex 3.10 *)
+
+datatype instr = LOADI val | LOAD vname | ADD
+
+type_synonym stack = "val list"
+
+fun exec1 :: "instr \<Rightarrow> state \<Rightarrow> stack \<Rightarrow> stack option" where
+  "exec1 (LOADI n) _ stk = Some (n # stk)" |
+  "exec1 (LOAD x ) s stk = Some (s(x) # stk)" |
+  "exec1 ADD _ (j # i # stk ) = Some ((i + j) # stk)" |
+  "exec1 ADD _ _  = None"
+
+fun exec :: "instr list \<Rightarrow> state \<Rightarrow> stack \<Rightarrow> stack option" where
+  "exec [] _ stk = Some stk" |
+  "exec (i # is) s stk =
+    (case (exec1 i s stk) of
+      (Some s') \<Rightarrow> (exec is s s') |
+      None \<Rightarrow> None)"
+
+fun comp :: "aexp \<Rightarrow> instr list" where
+  "comp (N n)  = [LOADI n]" |
+  "comp (V x ) = [LOAD x ]" |
+  "comp (Plus e1 e2 ) = comp e1 @ comp e2 @ [ADD]"
+
+lemma exec_lemma :
+  "exec is1 s stk = Some s' \<Longrightarrow> exec (is1 @ is2) s stk = exec is2 s s'"
+  apply(induction is1 arbitrary: stk)
+  apply(auto split: option.split)
+  done
+
+theorem "exec (comp a) s stk = Some (aval a s # stk) "
+  apply(induction a arbitrary: stk)
+  apply(auto simp add: exec_lemma)
+  done
 
 
 end
